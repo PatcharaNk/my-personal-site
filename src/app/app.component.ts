ce7485fd7a863e8c21bdc9a84dd1291282cd4bf9
PatcharaNk <naka.patchara@gmail.com>
2018-09-04 17:35:53 +0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-personal-site';
+  status: boolean;
+  openNav() {
+    this.status = true;
+  }
+  closeNav() {
+    this.status = false;
+  }
+  setNav() {
+    const style = {
+      'width': this.status ? '250px' : '0px',
+    };
+    return style;
+  }
 }
